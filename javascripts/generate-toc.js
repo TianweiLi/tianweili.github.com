624d17b4eb09062e0b58218163e4a65d61035013
TianweiLi <litianwei2013@gmail.com>
2015-02-17 14:56:58 +0800

    Site updated at 2015-02-17 06:56:57 UTC

--- javascripts/generate-toc.js
+++ javascripts/generate-toc.js
@@ -1,15 +1,28 @@
-function generateTOC(insertBefore, heading, startLv) {
-    var container = jQuery("<div id='tocBlock'></div>");
-    var div = jQuery("<ul id='toc'></ul>");
-    var content = jQuery(insertBefore).first();
-
-    if (heading !== undefined && heading !== null) {
-        container.append('<span class="tocHeading">' + heading + '</span>');
-    }
-
-    if (startLv === undefined) { startLv = 1; }
-
-    div.tableOfContents(content, { startLevel: startLv });
-    container.append(div);
-    container.insertBefore(insertBefore);
-}
+$(document).ready(function() {
+	if($('#comments').length) {
+		var div = $("div.entry-content");
+		var tocBlock = $("<div id='tocBlock'><span class='tocHeading'>目录</span></div>");
+		var ul = $("<ul id='toc'></ul>");
+		
+		div.prepend(tocBlock);
+		tocBlock.append(ul);
+		
+		div.find(":header").each(function(i){
+			if($(this).prop("tagName") == 'H2') {
+				var h2 = $("<li><a href='#h2" + i + "'>"+ $(this).text() +"</a></li>");
+				ul.append(h2);
+				$(this).attr('id', 'h2' + i);
+			}
+			if($(this).prop("tagName") == 'H3') {
+				var li_last = $("#toc").children().last();
+				
+				if(!li_last.has('ul').length) {
+					$("#toc").children().last().append("<ul></ul>");
+				}
+				
+				li_last.children('ul').first().append("<li><a href='#h3" + i + "'>"+ $(this).text() +"</a></li>");
+				$(this).attr('id', 'h3' + i);
+			}
+		});
+	}
+});
