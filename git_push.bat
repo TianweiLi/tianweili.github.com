18238a2fb8d2e9ae7832a7628db91e3b3801b564
TianweiLi <litianwei2013@gmail.com>
2015-02-17 22:34:26 +0800

    delete some files

--- git_push.bat
+++
@@ -1,9 +0,0 @@
-cd C:\Users\jili\Documents\GitHub\tianweili.github.com\
-
-echo push start...
-git pull origin source
-git add .
-git commit -m 'blog source'
-git push origin source
-echo push success!
-pause
